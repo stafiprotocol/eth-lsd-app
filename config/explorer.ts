c7310ae36d146b8a551057953a8125027c7f6589
joe <joewu590@gmail.com>
2023-12-27 21:43:25 +0800

    use holesky instead of goerli

--- config/explorer.ts
+++ config/explorer.ts
@@ -2,28 +2,28 @@ import { isDev } from "./env";
 
 export function getEtherScanUrl() {
   if (isDev()) {
-    return `https://goerli.etherscan.io`;
+    return `https://holesky.etherscan.io`;
   }
   return `https://etherscan.io`;
 }
 
 export function getEtherScanTxUrl(txHash: string | undefined) {
   if (isDev()) {
-    return `https://goerli.etherscan.io/tx/${txHash}`;
+    return `https://holesky.etherscan.io/tx/${txHash}`;
   }
   return `https://etherscan.io/tx/${txHash}`;
 }
 
 export function getEtherScanAccountUrl(account: string) {
   if (isDev()) {
-    return `https://goerli.etherscan.io/address/${account}`;
+    return `https://holesky.etherscan.io/address/${account}`;
   }
   return `https://etherscan.io/address/${account}`;
 }
 
 export function getEtherScanErc20TxUrl(address: any) {
   if (isDev()) {
-    return `https://goerli.etherscan.io/address/${address}#tokentxns`;
+    return `https://holesky.etherscan.io/address/${address}#tokentxns`;
   }
   return `https://etherscan.io/address/${address}#tokentxns`;
 }
