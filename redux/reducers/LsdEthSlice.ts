924204234ece0424873551e9c844410d3865c2e5
SergeevDmitry <sergeevdms@gmail.com>
2024-12-10 12:19:42 +0100

    Calculate APR for last 5 days

--- redux/reducers/LsdEthSlice.ts
+++ redux/reducers/LsdEthSlice.ts
@@ -121,7 +121,7 @@ export const updateApr = (): AppThunk => async (dispatch, getState) => {
 
     const eraSeconds = Number(updateBalancesEpochs) * (getBlockSeconds() * 32);
 
-    const eventLength = Math.round((7 * 24 * 3600) / eraSeconds);
+    const eventLength = Math.round((5 * 24 * 3600) / eraSeconds);
 
     const topics = web3.utils.sha3(
       "BalancesUpdated(uint256,uint256,uint256,uint256)"
